51b35d3f2db54c68760789160ce0e64c408905c8
Soumyajeet-Muni <117106268+Soumyajeet-Muni@users.noreply.github.com>
2023-05-18 01:04:53 +0530

    Update login.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -9,7 +9,7 @@ function Login() {
     username: 'default',
     email: 'default@gmail.com',
     occupation: 'student',
-    gender: '',
+    gender: 'Male',
   })
 
   const onChangeHandler = (event) => {
@@ -29,52 +29,48 @@ function Login() {
   
   }
   return (
-    <>
-    
     <div className="login" >
-    <h1 className='header' style={{color:"blue"}} > User Login</h1>
-     <form className='form' onSubmit={onSubmitHandler}>
-      <div className="form-group1">
-        <label htmlFor="username" className="form-label1">User Name</label>
-        <input className="form-control1" name="username" onChange={onChangeHandler} value={formData.username} />
+     <h1 className='header' style={{color:"blue"}} > User Login </h1>
+    <form className='form' onSubmit={onSubmitHandler}>
+     <div className="form-group1">
+     <label htmlFor="username" className="form-label1">User Name</label>
+     <input className="form-control1" name="username" onChange={onChangeHandler} value={formData.username} />
+     </div>
+     <div className="form-group1">
+     <label htmlFor="email" className="form-label1">Email</label>
+     <input className="form-control1" name="email" onChange={onChangeHandler} value={formData.email} />
+     </div>
+    <div className="form-group1">
+     <label htmlFor="occupation" className="form-label1">Occupation</label>
+     <select className="form-select1" name="occupation" onChange={onChangeHandler} value={formData.occupation}>
+     <option value="student">Student</option>
+    <option value="employee">Employee</option>
+    <option value="other">Other</option>
+     </select>
+     </div>
+   <div className="form-group1">
+     <label htmlFor="gender" className="form-label1">Gender</label>
+    <div className="label">
+  <div>
+     <input type="radio" name="gender" value="male" onChange={onChangeHandler} checked={formData.gender === 'male'} />
+     <label htmlFor="male">Male</label>
       </div>
-      <div className="form-group1">
-        <label htmlFor="email" className="form-label1">Email</label>
-        <input className="form-control1" name="email" onChange={onChangeHandler} value={formData.email} />
-      </div>
-      <div className="form-group1">
-        <label htmlFor="occupation" className="form-label1">Occupation</label>
-        <select className="form-select1" name="occupation" onChange={onChangeHandler} value={formData.occupation}>
-          <option value="student">Student</option>
-          <option value="employee">Employee</option>
-          <option value="other">Other</option>
-        </select>
-      </div>
-      <div className="form-group1">
-        <label htmlFor="gender" className="form-label1">Gender</label>
-        <div>
-          <div>
-            <input type="radio" name="gender" value="male" onChange={onChangeHandler} checked={formData.gender === 'male'} />
-            <label htmlFor="male">Male</label>
-          </div>
-          <div>
-            <input type="radio" name="gender" value="female" onChange={onChangeHandler} checked={formData.gender === 'female'} />
-            <label htmlFor="female">Female</label>
-          </div>
-          <div>
-            <input type="radio" name="gender" value="other" onChange={onChangeHandler} checked={formData.gender === 'other'} />
-            <label htmlFor="other">Other</label>
-          </div>
-        </div>
-      </div>
-      
-      <div className="form-group1">
-        <button className="btn" type="submit" onClick={() => navigate('/dashboard',{state:{name:formData.username,occupation:formData.occupation,gender:formData.gender}})} > Submit</button>
-      </div>
-    </form>
-  </div>  
+     <div>
+     <input type="radio" name="gender" value="female" onChange={onChangeHandler} checked={formData.gender === 'female'} />
+     <label htmlFor="female">Female</label>
+   </div>
+   <div>
+     <input type="radio" name="gender" value="other" onChange={onChangeHandler} checked={formData.gender === 'other'} />
+     <label htmlFor="other">Other</label>
+     </div>
+     </div>
+    </div>
     
-    </>
+     <div className="form-group1">
+     <button className="btn" type="submit" onClick={() => navigate('/dashboard',{state:{name:formData.username,occupation:formData.occupation,gender:formData.gender}})} > Submit</button>
+    </div>
+    </form>
+    </div> 
     
   );
 }
